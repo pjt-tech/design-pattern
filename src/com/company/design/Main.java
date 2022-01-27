beedc61692ecfb7f166ececdc09533a6fd05652e
jintae_park <dktkd7044@gmail.com>
2022-01-27 20:07:31 +0900

    proxy, decorator 패턴 실습

--- src/com/company/design/Main.java
+++ src/com/company/design/Main.java
@@ -1,6 +1,13 @@
 package com.company.design;
 
 import com.company.design.adapter.*;
+import com.company.design.aop.AopBrowser;
+import com.company.design.decorator.*;
+import com.company.design.proxy.Browser;
+import com.company.design.proxy.BrowserProxy;
+import com.company.design.proxy.IBrowser;
+
+import java.util.concurrent.atomic.AtomicLong;
 
 public class Main {
 
@@ -14,7 +21,9 @@ public class Main {
         SocketClient bClient = bClazz.getSocketClient();
 
         System.out.println(aClient.equals(bClient));*/
-        HairDryer hairDryer = new HairDryer();
+
+        /* 어댑터 패턴 테스트
+        Electronic110V hairDryer = new HairDryer();
         connect(hairDryer);
 
         Cleaner cleaner = new Cleaner();
@@ -22,8 +31,45 @@ public class Main {
         connect(adapter);
         AirConditioner airConditioner = new AirConditioner();
         Electronic110V airAdapter = new SocketAdapter(airConditioner);
-        connect(airAdapter);
+        connect(airAdapter);*/
 
+        /* proxy 패턴 aop활용 실습
+        BrowserProxy browser = new BrowserProxy("www.naver.com");
+        browser.show();
+        browser.show();
+        browser.show();
+        browser.show();
+
+        AtomicLong start = new AtomicLong();
+        AtomicLong end = new AtomicLong();
+
+        IBrowser aopBrowser = new AopBrowser("www.naver.com",
+                ()->{
+                    System.out.println("before");
+                    start.set(System.currentTimeMillis());
+                },
+                ()->{
+                    long now = System.currentTimeMillis();
+                    end.set(now - start.get());
+                });
+        aopBrowser.show();
+        System.out.println("loading time : " + end.get());
+
+        aopBrowser.show();
+        System.out.println("loading time : " + end.get());
+        */
+        /* Decorator 패턴 테스트
+        ICar audi = new Audi(1000);
+        audi.showPrice();
+        //a3
+        ICar audi3 = new A3(audi, "A3");
+        audi3.showPrice();
+        //a4
+        ICar audi4 = new A4(audi, "A4");
+        audi4.showPrice();
+        //a5
+        ICar audi5 = new A5(audi, "A5");
+        audi5.showPrice();*/
     }
 
     //콘센트
